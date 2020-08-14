f8c8c3fad9a32463bc7f85fa3c9adf7095db6cce
zubeda <zubedauk@gmail.com>
2020-08-14 17:19:24 +0100

    add time

--- src/index.js
+++ src/index.js
@@ -9,20 +9,33 @@ let limitMsg;
 let wordCount=0;
 let firstWord=false;
 btnSubmit.addEventListener("click",function(event){
+    let time=new Date();
     if(msg.value!=""){
         event.preventDefault();
         let paraEl=document.createElement("p");
         let btnDel=document.createElement("button");
+        let hr=document.createElement("hr");
+        let br=document.createElement("br");
+        let cont=document.createElement("div");
+        let timeSpace=document.createElement("p");
         paraEl.className="para";
         btnDel.className="del";
         btnDel.innerHTML="delete";
-        paraEl.textContent=msg.value;
+        timeSpace.innerText="zubeda khanum @zubedauk:"+time.getSeconds()+" seconds ago";
+        paraEl.innerHTML=msg.value;
         paraEl.appendChild(btnDel);
-        tweet.appendChild(paraEl);
+        cont.appendChild(timeSpace);
+        cont.appendChild(paraEl);
+       // cont.appendChild(btnDel);
+        cont.appendChild(hr);
+       tweet.appendChild(cont);
+       // tweet.appendChild(hr);
         
     
     btnDel.addEventListener("click",function(e){
-        btnDel.parentNode.remove();
+       // btnDel.parentNode.remove();
+        btnDel.parentElement.parentElement.remove();
+        hr.remove();
        
  })}
 
@@ -32,7 +45,7 @@ btnSubmit.addEventListener("click",function(event){
 })
  
 msg.addEventListener("keyup",function(e){
-        console.log(msg.value.length);
+        console.log(e.keyCode);
         arrTweet=msg.value.split(" ");
 
         words.textContent=msg.value.length;
@@ -40,6 +53,7 @@ msg.addEventListener("keyup",function(e){
             arrTweet=msg.value.split(" ");
             console.log(arrTweet);
         }
+        
         if(msg.value.length>280){
             msg.style.backgroundColor="red";
         }
